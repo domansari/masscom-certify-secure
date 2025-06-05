e7e0b4086d99e295c11d4702b5a21021a9a32fcb
domansari <info@masscom.co.in>
2025-06-05 13:00:24 +0530

    Updated CertificateBody.tsx

--- src/components/certificate/CertificateBody.tsx
+++ src/components/certificate/CertificateBody.tsx
@@ -73,7 +73,7 @@ const CertificateBody = ({ data }: CertificateBodyProps) => {
         <img 
           src="/lovable-uploads/dc9bc102-52f2-4cb0-ae96-01e99ea9fc4f.png" 
           alt="Official Seal" 
-          className="h-48 w-48 object-contain opacity-30"
+          className="h-48 w-48 object-contain opacity-100"
         />
       </div>
 
