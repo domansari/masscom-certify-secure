6ac5fca8e7e284534a936d0f1d40a2975c32188e
domansari <info@masscom.co.in>
2025-06-05 20:04:26 +0530

    Update CertificatePreview.tsx
    G

--- src/components/CertificatePreview.tsx
+++ src/components/CertificatePreview.tsx
@@ -19,7 +19,7 @@ const CertificatePreview = ({ data }: CertificatePreviewProps) => {
           padding: '0',
           margin: '0',
           fontFamily: 'Times, serif',
-          backgroundImage: `url('./lovable-uploads/axa.png')`,
+          backgroundImage: `url('./public/lovable-uploads/axa.png')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
