ba24860447d910b2ab7456bcde2c5c95621283a4
domansari <info@masscom.co.in>
2025-06-06 13:55:33 +0530

    Update CertificateBody.tsx

--- src/components/certificate/CertificateBody.tsx
+++ src/components/certificate/CertificateBody.tsx
@@ -11,6 +11,10 @@ const CertificateBody = ({ data }: CertificateBodyProps) => {
       {/* Certificate Title */}
     
       
+           <div className="text-center mb-5">
+              <p className="text-2xl text-gray-800 mt-2"></p>
+        <p className="text-2xl text-gray-800 mt-2"></p>
+      </div>
       
       <div className="text-center mb-5">
               <p className="text-2xl text-gray-800 mt-2"></p>
