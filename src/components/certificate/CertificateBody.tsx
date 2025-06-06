003297900a972c92d4b3296f90a900d2d8990f7a
domansari <info@masscom.co.in>
2025-06-06 12:19:13 +0530

    Update CertificateBody.tsx

--- src/components/certificate/CertificateBody.tsx
+++ src/components/certificate/CertificateBody.tsx
@@ -10,9 +10,7 @@ const CertificateBody = ({ data }: CertificateBodyProps) => {
     <>
       {/* Certificate Title */}
       <div className="text-center mb-1">
-        <h2 className="text-6xl font-bold italic tracking-widest mb-3" style={{ color: '#b48811' }}>
-          Certificate
-        </h2>
+      
         <p className="text-2xl text-gray-800 mt-2">of Achievement, This is to certify that</p>
       </div>
 
