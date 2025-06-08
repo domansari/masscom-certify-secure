964f0ace0e12fa8db2e73de9d9f2120f09cf284a
domansari <info@masscom.co.in>
2025-06-08 14:05:51 +0530

    Update CertificateFooter.tsx

--- src/components/certificate/CertificateFooter.tsx
+++ src/components/certificate/CertificateFooter.tsx
@@ -19,7 +19,7 @@ const CertificateFooter = ({ data }: CertificateFooterProps) => {
             </div>
           </div>
           <div className="text-center flex-1 mx-12">
-            <div className="border-t-2 border-black pt-3 mt-6">
+            <div className="border-t-2 border-black pt-3 mt-8">
               <p className="text-sm text-black-800 mb-1">
                 Akbar Ansari
               </p>
