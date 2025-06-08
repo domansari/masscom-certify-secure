d3192cebf86b53ff0c5bcafa7a15017876760ae0
domansari <info@masscom.co.in>
2025-06-08 14:09:55 +0530

    Update CertificateHeader.tsx

--- src/components/certificate/CertificateHeader.tsx
+++ src/components/certificate/CertificateHeader.tsx
@@ -9,7 +9,7 @@ const CertificateHeader = ({ data }: CertificateHeaderProps) => {
   return (
     <>
       {/* Certificate ID - Extreme Left Top Corner */}
-      <div className="absolute top-2 left-4 text-sm font-bold text-black z-10">
+      <div className="absolute top-1 left-4 text-sm font-bold text-black z-10">
         Certificate ID: {data.certificateId || "Generated ID"}
       </div>
 
@@ -38,10 +38,7 @@ const CertificateHeader = ({ data }: CertificateHeaderProps) => {
               <p className="text-2xl text-gray-800 mt-2"></p>
         <p className="text-2xl text-gray-800 mt-2"></p>
       </div>
-       <div className="text-center mb-5">
-              <p className="text-2xl text-gray-800 mt-2"></p>
-        <p className="text-2xl text-gray-800 mt-2"></p>
-      </div>
+     
       {/* Subtitle */}
       <div className="text-center mb-5">
         <p className="text-lg italic text-black-600">
