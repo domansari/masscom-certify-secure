9b325981e528b1e40c53602948e0cde244dae529
domansari <info@masscom.co.in>
2025-06-08 13:34:13 +0530

    Update CertificateBody.tsx

--- src/components/certificate/CertificateBody.tsx
+++ src/components/certificate/CertificateBody.tsx
@@ -95,8 +95,8 @@ const CertificateBody = ({ data }: CertificateBodyProps) => {
         <div className="text-center">
           <p className="text-x1 font-semibold text-black-700 mb-3">Duration:
               {data.duration ? `${data.duration}` : "Duration"}Grade:{data.grade || "Grade"}Date of Issue:{data.completionDate ? new Date(data.completionDate).toLocaleDateString() : "Date"}
-         </p>      </div>
- <div className="border-b border-black pb-1 px-4">
+         </p>    <div className="border-b border-black pb-1 px-4">   </div>
+
 
       
       </div>      </div>
